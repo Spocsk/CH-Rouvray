084d4d9f8c22257c03ad38877905f332de6a95f1
Dylan COUTO DE OLIVEIRA <dcoutodeoliveira@normandiewebschool.fr>
2022-02-14 10:06:30 +0100

    Ajout d'une table pour les uploads

---
+++ src/Entity/MediaObject.php
@@ -0,0 +1,97 @@
+<?php
+// api/src/Entity/MediaObject.php
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Controller\CreateMediaObjectActionController;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+
+
+/**
+ * @ORM\Entity
+ * @Vich\Uploadable
+ */
+#[ApiResource(
+    collectionOperations: [
+        'get',
+        'post' => [
+            'controller' => CreateMediaObjectActionController::class,
+            'deserialize' => false,
+            'validation_groups' => ['Default', 'media_object_create'],
+            'openapi_context' => [
+                'requestBody' => [
+                    'content' => [
+                        'multipart/form-data' => [
+                            'schema' => [
+                                'type' => 'object',
+                                'properties' => [
+                                    'file' => [
+                                        'type' => 'string',
+                                        'format' => 'binary',
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+    itemOperations: ['get'],
+    normalizationContext: ['groups' => ['media_object:read']]
+)]
+class MediaObject
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     * @ORM\Id
+     */
+    private ?int $id = null;
+
+    #[Groups(['media_object:read'])]
+    public ?string $contentUrl = null;
+
+    /**
+     * @Vich\UploadableField(mapping="media_object", fileNameProperty="filePath")
+     */
+    #[Assert\NotNull(groups: ['media_object_create'])]
+    public ?File $file = null;
+
+    /**
+     * @ORM\Column(nullable=true)
+     */
+    public ?string $filePath = null;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="uploadedFiles")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+}

e139fe426aed081cd676c5cf48ab392ebd9d7420
Dylan COUTO DE OLIVEIRA <dcoutodeoliveira@normandiewebschool.fr>
2022-02-14 09:41:30 +0100

    Ajout d'une table Comment

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -2,12 +2,14 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\CommentRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=CommentRepository::class)
  */
+#[ApiResource()]
 class Comment
 {
     /**
@@ -17,8 +19,43 @@ class Comment
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=News::class, inversedBy="comments")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $news;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getNews(): ?News
+    {
+        return $this->news;
+    }
+
+    public function setNews(?News $news): self
+    {
+        $this->news = $news;
+
+        return $this;
     }
 }
